5904bd26db100d378e008780ed42d9ca6d97de6d
Yahya Al-Shamali <42281247+PointlessUser@users.noreply.github.com>
2025-03-24 16:49:28 -0600

    Add Conda Support

---
+++ conda_recipe/build.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+rm -rf build
+mkdir -p build
+cd build
+cmake -DCMAKE_CXX_COMPILER=${CXX} -DBUILD_SPARSE_CCJ=OFF ..
+cmake --build . --parallel
+cmake --install . --prefix=${PREFIX}
+
+mkdir -p $PREFIX/share/simfold/params
+cp -r $SRC_DIR/simfold/params/* $PREFIX/share/simfold/params/
